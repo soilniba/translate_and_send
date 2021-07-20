d3ca64667cced88f9ff76071c094cb5d4819e017
王畅 <wangchang@aurogon.com>
2021-07-20 20:23:03 +0800

    测试好啦补完一下

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 import json
 import demjson
 
-data = b'{\n  "Text": "kankan kankan  test  test 122  aabb",\n  "UserName": "dFKq3rPdUrf2ifu",\n  "LinkToTweet": "http://twitter.com/dFKq3rPdUrf2ifu/status/1417423117723471872",\n  "TweetEmbedCode": "<blockquote class="twitter-tweet">\n  <p lang="en" dir="ltr">kankan kankan  test  test 122  aabb</p>\n  &mdash; \xe7\x9c\x8b\xe7\x9c\x8b (@dFKq3rPdUrf2ifu)\n  <a href="https://twitter.com/dFKq3rPdUrf2ifu/status/1417423117723471872">Jul 20, 2021</a>\n</blockquote>\n<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\n",\n  "CreatedAt": "July 20, 2021 at 05:56PM"\n}'
+data = b'{\n  "Text": "kankan kankan  test  test 122  aabb",\n  "UserName": "dFKq3rPdUrf2ifu",\n  "LinkToTweet": "http://twitter.com/dFKq3rPdUrf2ifu/status/1417423117723471872",\n  "CreatedAt": "July 20, 2021 at 05:56PM"\n}'
 
 utf8 =data.decode('utf-8') 
-# utf8 = utf8.replace('\r', '\\r').replace('\n', '\\n')
+# utf8 = utf8.replace('\n', '')
 print(utf8)
-print(demjson.encode(data))
+# json_table = demjson.decode(data)
+json_table = json.loads(data)
+print(json_table)
+print(json_table['Text'])
--- translate_and_send.py
+++ translate_and_send.py
@@ -1,5 +1,5 @@
 import json
-import demjson
+# import demjson
 import random
 import uuid
 import hashlib
@@ -26,14 +26,12 @@ def translate():
             print(request.get_data().decode('utf-8'))
             print(request.get_json(force=True))
             # json_table = request.get_json(force=True)
-            json_table = demjson.encode(request.get_data().decode('utf-8'))
+            # json_table = demjson.encode(request.get_data())
+            json_table = json.loads(request.get_data())
             print(json.dumps(json_table))
-            # FromLang = json_table['FromLang']
-            # ToLang = json_table['ToLang']
             Text = json_table['Text']
             UserName = json_table['UserName']
             LinkToTweet = json_table['LinkToTweet']
-            TweetEmbedCode = json_table['TweetEmbedCode']
             CreatedAt = json_table['CreatedAt']
 
             try:
